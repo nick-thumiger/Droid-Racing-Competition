33d4998974e14d2033ddc36cea9061495af7e2b9
VibraniumWarrior97 <67862197+VibraniumWarrior97@users.noreply.github.com>
2020-07-13 23:05:31 +1000

    Update laneDetection.m
    Added seperate left and right lane detection features.

--- laneDetection.m
+++ laneDetection.m
@@ -1,48 +1,87 @@
-function [laneEdge, BW, maskedRGBImage] = laneDetection(image)
+function [leftColourMask, leftBinaryMask, leftEdge, rightColourMask, rightBinaryMask, rightEdge, bothEdges, bothBinary, bothColour] = laneDetection()
 %Read image
-RGBimage = imread(image);
+RGBimage = imread('v_upcoming.jpg');
 %Extract binary mask covering the lanes
-[BW, maskedRGBImage] = createMask(RGBimage);
-figure(); imshow(maskedRGBImage);
+[leftEdge, leftBinaryMask, leftColourMask] = blueMask(RGBimage);
+[rightEdge, rightBinaryMask, rightColourMask] = redMask(RGBimage);
 
-%Clear any noise on the binary mask
-cleanUp = bwareaopen(BW, 500);
-figure(2); imshow(cleanUp);
+bothEdges = or(leftEdge, rightEdge);
+bothBinary = or(leftBinaryMask, rightBinaryMask);
+bothColour = or(leftColourMask, rightColourMask);
+end
+
+function [laneEdge, BW,maskedRGBImage] = blueMask(image)
+% Convert RGB image to the ycbcr color space
+I = rgb2ycbcr(image);   
+
+
+% Define thresholds for Y Channel based on histogram settings
+YchannelMin = 0.000; YChannelMax = 154.000;
+
+% Define thresholds for Cb Channel based on histogram settings
+CbChannelMin = 175.000; CbChannelMax = 255.000;
+
+% Define thresholds for Cr Channel based on histogram settings
+CrChannelMin = 107.000; CrChannelMax = 140.000;
+
+% Create mask based on chosen histogram thresholds
+BW = (I(:,:,1) >= YchannelMin ) & (I(:,:,1) <= YChannelMax) & ...
+     (I(:,:,2) >= CbChannelMin ) & (I(:,:,2) <= CbChannelMax) & ...
+     (I(:,:,3) >= CrChannelMin ) & (I(:,:,3) <= CrChannelMax);
+
+% Initialize output masked image based on input image.
+maskedRGBImage = image;
+
+% Set background pixels where BW is false to zero.
+maskedRGBImage(repmat(~BW,[1 1 3])) = 0;
+
+
+cleanUp = bwareaopen(BW, 900);
 
 %Dilate the binary mask a little bit to get rid of gaps caused due to the
 %grid. Structural element is set to a very small value so this does not
 %affect the size of the lanes in the mask
 se = strel('line', 15, 10);
 dilatedMask = imdilate(cleanUp, se);
-figure(3); imshow(dilatedMask);
 
 %Use sobel operators to extract edges from the binary mask
 laneEdge = edge(dilatedMask, 'Sobel');
-figure(4); imshow(laneEdge);
 end
 
-function [BW,maskedRGBImage] = createMask(image)
-% Convert RGB image to the lab colour space.
-I = rgb2lab(image);
 
-% Define thresholds for channel 1 based on histogram settings
-lMin = 21.017; lMax = 82.729;
+function [laneEdge, BW,maskedRGBImage] = redMask(image)
+% Convert RGB image to YCbCr colour space
+I = rgb2ycbcr(image);
 
-% Define thresholds for channel 2 based on histogram settings
-aMin = 9.549; aMax = 78.012;
 
-% Define thresholds for channel 3 based on histogram settings
-bStarMin = -106.228; bStarMax = 41.205;
+% Define thresholds for Y Channel based on histogram settings
+YchannelMin = 0.000; YchannelMax = 106.000;
+
+% Define thresholds for Cb Channel based on histogram settings
+CbChannelMin = 82.000; CbChannelMax = 121.000;
+
+% Define thresholds for Cr Channel based on histogram settings
+CrChannelMin = 157.000; CrChannelMax = 200.000;
 
 % Create mask based on chosen histogram thresholds
-filter = (I(:,:,1) >= lMin ) & (I(:,:,1) <= lMax) & (I(:,:,2) >= aMin ) & (I(:,:,2) <= aMax) & (I(:,:,3) >= bStarMin ) & (I(:,:,3) <= bStarMax);
-BW = filter;
+BW = (I(:,:,1) >= YchannelMin ) & (I(:,:,1) <= YchannelMax) & ...
+           (I(:,:,2) >= CbChannelMin ) & (I(:,:,2) <= CbChannelMax) & ...
+           (I(:,:,3) >= CrChannelMin ) & (I(:,:,3) <= CrChannelMax);
 
-% Create a copy of the original image to help set the background pixels to
-% zero.
+% Initialize output masked image based on input image.
 maskedRGBImage = image;
 
 % Set background pixels where BW is false to zero.
 maskedRGBImage(repmat(~BW,[1 1 3])) = 0;
 
+cleanUp = bwareaopen(BW, 1000);
+
+%Dilate the binary mask a little bit to get rid of gaps caused due to the
+%grid. Structural element is set to a very small value so this does not
+%affect the size of the lanes in the mask
+se = strel('line', 15, 10);
+dilatedMask = imdilate(cleanUp, se);
+
+%Use sobel operators to extract edges from the binary mask
+laneEdge = edge(dilatedMask, 'Sobel');
 end
